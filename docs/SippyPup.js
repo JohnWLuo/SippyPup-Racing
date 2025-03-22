875556b602d01d3a73b2ecaa77d2b4be65e31f9d
JohnWLuo <john46luo@gmail.com>
2025-03-22 00:38:02 -0400

    Leaderboard fixed

--- docs/SippyPup.js
+++ docs/SippyPup.js
@@ -1,6 +1,14 @@
 class SippyPup {
-    constructor() {
-  
+    constructor(imageName, name, isFacingRight) {
+        this.imageName = imageName;
+        this.name = name;
+        this.isFacingRight = isFacingRight;
+        this.isMoving = false;
+        this.position = 0;
+        this.speed = 0;
+        this.animationFrameId = null;
+        this.timeInterval = null;
+        this.startTime = null;
     }
 
     getRandomSpeed() {
@@ -10,7 +18,7 @@ class SippyPup {
     changeSpeed() {
         if (!this.isMoving) return;
         this.speed = this.getRandomSpeed();
-        this.speedDisplay.textContent = this.speed.toFixed(2) + "%";
+        this.speedDisplay = this.speed.toFixed(2) + "%";
         setTimeout(this.changeSpeed.bind(this), Math.random() * 500 + 500);
     }
 
@@ -19,7 +27,7 @@ class SippyPup {
         this.position += this.speed / 100;
         this.img.style.left = this.position + "%";
         const scaledPosition = (this.position / (100 - (this.img.width / window.innerWidth) * 100)) * 100;
-        this.distanceDisplay.textContent = scaledPosition.toFixed(2) + "%";
+        this.distanceDisplay = scaledPosition.toFixed(2) + "%";
 
         if (this.onMove) this.onMove();
 
@@ -28,7 +36,7 @@ class SippyPup {
         } else {
             this.position = 100 - (this.img.width / window.innerWidth) * 100;
             this.img.style.left = this.position + "%";
-            this.distanceDisplay.textContent = "100%"; // Ensure distance shows 100%
+            this.distanceDisplay = "100%"; // Ensure distance shows 100%
             this.isMoving = false;
             clearInterval(this.timeInterval);
             if (this.onMove) this.onMove();
@@ -41,7 +49,7 @@ class SippyPup {
         this.startTime = Date.now();
         this.timeInterval = setInterval(() => {
             const elapsedTime = Date.now() - this.startTime;
-            this.timeDisplay.textContent = `Time: ${(elapsedTime / 1000).toFixed(3)}`;
+            this.timeDisplay = `Time: ${(elapsedTime / 1000).toFixed(3)}`;
         }, 10);
         this.moveImage();
         this.changeSpeed();
@@ -53,9 +61,9 @@ class SippyPup {
         clearInterval(this.timeInterval);
         this.position = 0;
         this.img.style.left = this.position + "%";
-        this.speedDisplay.textContent = "0%";
-        this.distanceDisplay.textContent = "0%";
-        this.timeDisplay.textContent = "0.000";
+        this.speedDisplay = "0%";
+        this.distanceDisplay = "0%";
+        this.timeDisplay = "0.000";
     }
 }
 
--- docs/SippyPupStables.js
+++ docs/SippyPupStables.js
@@ -1,8 +1,13 @@
 import SippyPup from './SippyPup.js';
 
 class SippyPupStables {
-    const 
-    SippyPupList = []
+    constructor() {
+        const hankyPanky = new SippyPup("hanky panky Head Shot.png", "Hanky Panky", false);
+        const lyn = new SippyPup("Lyn_Mame_Test.jpg", "Lyn Mametchi", false);
+        const palmesan = new SippyPup("Palmesan.png", "Palmesan", false);
+
+        this.SippyPupList = [hankyPanky, lyn, palmesan];
+    }
 }
 
 export const SippyPupList = new SippyPupStables().SippyPupList;
