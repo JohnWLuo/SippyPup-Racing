b60dc04dad9c598de77d199f52d6874ba09861d4
JohnWLuo <john46luo@gmail.com>
2025-04-23 18:23:30 -0400

    Small amendment to previous commit

--- docs/SippyPupStables.js
+++ docs/SippyPupStables.js
@@ -5,9 +5,9 @@ class SippyPupStables {
         const hankyPanky = new SippyPup("hanky panky Head Shot.png", "Hanky Panky", false);
         const lyn = new SippyPup("Lyn_Mame_Test.jpg", "Lyn Mametchi", false);
         const palmesan = new SippyPup("Palmesan.png", "Palmesan", false);
-        const lily = new SippyPup("Lily.jpg", "Lily", false);
-        const lynko = new SippyPup("Lynko.jpg", "Lynko", true);
-        const yan = new SippyPup("FF14 Yan.jpg", "Yan", false);
+        const lily = new SippyPup("Lily.png", "Lily", false);
+        const lynko = new SippyPup("Lynko.png", "Lynko", true);
+        const yan = new SippyPup("FF14 Yan.png", "Yan", false);
 
         this.SippyPupList = [hankyPanky, lyn, palmesan, lily, lynko, yan];
     }
